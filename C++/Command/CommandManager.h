82b55fc193955ffaa084b869bbc3aa1e9c9c2e47
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-29 04:34:40 -0500

    Create CommandManager.h

---
+++ C++/Command/CommandManager.h
@@ -0,0 +1,24 @@
+#ifndef COMMANDMANAGER_H
+#define COMMANDMANAGER_H
+
+#include <stack>
+#include "Core/Base.h"
+#include "Command/Command.h"
+
+typedef std::stack<UniquePtr<Command>> CommandStack;
+
+class CommandManager {
+	static CommandStack UndoStack;
+	static CommandStack RedoStack;
+
+public:
+	//template<typename T, typename ...Args>
+	//static void ExecuteA(T command, Args && ...args);
+	static void Init();
+	static void Execute(UniquePtr<Command> command);
+	static void Undo();
+	static void Redo();
+	static void Clear();
+	static bool IsRedoStackEmpty();
+};
+#endif //COMMANDMANAGER_H
